988b2ce8586239583842c46b9e8056f070e0c9d5
ztreuse <118502751+ztreuse@users.noreply.github.com>
2024-03-01 20:53:09 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mainMenuButton = document.getElementById('mainMenuButton');
+    var modal = document.getElementById('modalid');
+    var bootstrapModal = new bootstrap.Modal(modal, {
+        backdrop: 'static'
+    });
+
+    mainMenuButton.addEventListener('click', function() {
+        bootstrapModal.hide();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mainMenuButton = document.getElementById('mainMenuButton');
+    var modal = document.getElementById('modalid2');
+    var bootstrapModal = new bootstrap.Modal(modal, {
+        backdrop: 'static'
+    });
+
+    mainMenuButton.addEventListener('click', function() {
+        bootstrapModal.hide();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mainMenuButton = document.getElementById('mainMenuButton');
+    var modal = document.getElementById('modalid3');
+    var bootstrapModal = new bootstrap.Modal(modal, {
+        backdrop: 'static'
+    });
+
+    mainMenuButton.addEventListener('click', function() {
+        bootstrapModal.hide();
+    });
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
